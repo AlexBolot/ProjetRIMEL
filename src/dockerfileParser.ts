c6cc9ff43e880b8ea49c295c9d926d603f236f09
Theos <theos.mariani@gmail.com>
2020-01-19 16:26:19 +0100

    integration fail need help from coco

---
+++ src/dockerfileParser.ts
@@ -0,0 +1,12 @@
+import * as DockerfileParser from 'dockerfile-ast';
+import { AstExplorer} from "./astExplorer";
+
+
+export function parsingDockerfile(files){
+  console.log(files);
+  let content="";
+  for(var file in files){
+    //let AstExplorer = new AstExplorer(file);
+  }
+
+}
