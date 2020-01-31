dbd1f3b27aeb61ec9d41fa230af9590a7759332b
Alexandre Bolot <alexandre.bolot@etu.unice.fr>
2020-01-31 11:36:09 +0100

    Updating crawler.js

--- src/crawler.js
+++ src/crawler.js
@@ -11,23 +11,24 @@ main();
 
 async function main() {
 
-    let url = 'https://api.github.com/search/repositories?q=language:java&sort=stars&order=desc&per_page=2';
+    let url = 'https://api.github.com/search/repositories?q=language:java&per_page=100';
 
     const result = { text: 'Java\n' };
 
-    for (let i = 1; i < 3; i++) {
+    for (let i = 1; i < 8; i++) {
         await processPage(url + `&page=${i}`, result);
     }
 
     await save('java', result.text);
-    console.log('... Done');
 }
 
 async function processPage(url, out) {
     request(url, { json: true, headers: { 'User-Agent': 'AlexBolot' } }, async (err, res, body) => {
 
-        if (body === undefined || body.items === undefined)
+        if (body === undefined || body.items === undefined) {
+            console.log(`body or items undefined for ${getFullName()}`);
             return;
+        }
 
         items = body.items;
         console.log(`${items.length} - for - ${url}`);
@@ -36,19 +37,21 @@ async function processPage(url, out) {
 
             const smollURL = `https://raw.githubusercontent.com/${getFullName(j)}/master/Dockerfile`;
             request.get(smollURL, { headers: { 'User-Agent': 'AlexBolot' } }, (err, res, body) => {
-
-                if (body === undefined)
+                if (body === undefined) {
+                    console.log('body undefined');
+                    console.log(err);
                     return;
+                }
 
                 if (body.trim() !== '404: Not Found') {
-                    out.text += `https://github.com/${getFullName(j)}\n`;
-                }
+                    out.text += `${getStars(j)}, https://github.com/${getFullName(j)}\n`;
+                } else console.log(`${getFullName(j)} has no dockerfile`);
+
             });
         }
     });
 
 }
-
 
 function getStars(index) {
     return items[index]['stargazers_count'];
